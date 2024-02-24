8ef106c00abc9ffee0d7bf760c24066e87121cce
Rajibur Rahman <Rajiburrahman74@gmail.com>
2024-02-24 16:13:12 +0600

    added

---
+++ pages/index.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+const links = [{
+  label: 'Home',
+  icon: 'i-heroicons-home'
+}, {
+  label: 'Inbox',
+  icon: 'i-heroicons-inbox',
+  badge: '4'
+}, {
+  label: 'Users',
+  icon: 'i-heroicons-user-group'
+}, {
+  label: 'Settings',
+  icon: 'i-heroicons-cog-8-tooth',
+  children: [{
+    label: 'General'
+  }, {
+    label: 'Members'
+  }, {
+    label: 'Notifications'
+  }]
+}]
+</script>
+
+<template>
+  <UDashboardPanel :width="300" :resizable="{ min: 200, max: 400 }">
+    <UDashboardNavbar>
+      <template #left>
+        <Logo class="w-auto h-5" />
+      </template>
+    </UDashboardNavbar>
+
+    <UDashboardSidebar>
+      <UDashboardSidebarLinks :links="links" />
+    </UDashboardSidebar>
+  </UDashboardPanel>
+</template>
